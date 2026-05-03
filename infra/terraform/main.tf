91317b580c192fa92f12ede25420388b2f4488f3
rohanpatel2002 <rohanpatel2118@gmail.com>
2026-05-03 08:58:08 +0530

    feat(infra): add Terraform modules for secure IronClad cloud infrastructure

---
+++ infra/terraform/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "ironclad-vpc"
+  cidr   = "10.0.0.0/16"
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "ironclad-prod"
+  cluster_version = "1.27"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 3
+      min_size     = 1
+      max_size     = 5
+      instance_types = ["t3.medium"]
+    }
+  }
+}
