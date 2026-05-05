c33023e5a8df2f46ac856b23af8e24feeb013193
rohanpatel2002 <rohanpatel2118@gmail.com>
2026-05-05 23:26:41 +0530

    feat(infra): add node group and cluster variables for EKS deployment

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -9,3 +9,21 @@ variable "environment" {
   type        = string
   default     = "prod"
 }
+
+variable "eks_node_min_size" {
+  description = "Minimum number of nodes in EKS cluster"
+  type        = number
+  default     = 3
+}
+
+variable "eks_node_max_size" {
+  description = "Maximum number of nodes in EKS cluster"
+  type        = number
+  default     = 10
+}
+
+variable "eks_instance_types" {
+  description = "Instance types for EKS node group"
+  type        = list(string)
+  default     = ["t3.large", "t3.xlarge"]
+}
